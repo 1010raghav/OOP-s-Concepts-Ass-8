a629ea8e68fb819c2d7d969723dc36071ef54c84
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:17:59 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("UC6 output");
+            
+            UC6_CalculateWageTill100HrsOr20IsReached obj6 = new UC6_CalculateWageTill100HrsOr20IsReached();
+            obj6.CalculateWageTill100HrsOr20IsReached();
+            
+           } 
+     }         
+} 
