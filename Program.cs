470668cdad7080fa7672fc10a8e4d0e4d8492b3b
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:21:26 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("UC8 output");
+            
+            UC8_ComputeEmpWageForMultipleCompanies obj8 = new UC8_ComputeEmpWageForMultipleCompanies();
+            obj.ComputeEmpWage("TCS", 10, 2, 8);
+            
+           } 
+     }         
+} 
