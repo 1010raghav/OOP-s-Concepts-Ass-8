686d05c8fcd922532de2e13eaa0db8d8e250448e
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:12:37 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("UC2 output");
+            
+            UC2_EmpWage obj2 = new UC2_EmpWage();
+            obj2.EmpWage();
+            
+           } 
+     }         
+} 
