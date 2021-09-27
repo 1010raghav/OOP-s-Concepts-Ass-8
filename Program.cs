8844d300930cfaa77d1fbce91b3a61ca6d84bcb5
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:19:23 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("UC7 output");
+            
+            UC7_ComputeEmpWageUsingClassMethod obj7 = new UC7_ComputeEmpWageUsingClassMethod();
+            obj7.ComputeEmpWage();
+           } 
+     }         
+} 
