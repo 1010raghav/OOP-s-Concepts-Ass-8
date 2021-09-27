84b7f145659d917cb61a0e79ef7c00f5d49a158f
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:14:17 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           Console.WriteLine("UC3 output");
+
+            UC3_PartTimeEmpWage obj3 = new UC3_PartTimeEmpWage();
+            obj3.PartTimeEmpWage();
+
+            
+           } 
+     }         
+} 
