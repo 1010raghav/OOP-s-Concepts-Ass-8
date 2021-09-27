4a4c142d8eb382410a976cde4c68d10283872b27
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:14:59 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           Console.WriteLine("UC4 output");
+            
+            UC4_EmpWageUsingSwitch obj4 = new UC4_EmpWageUsingSwitch();
+            obj4.EmpWageUsingSwitch();
+            
+           } 
+     }         
+} 
