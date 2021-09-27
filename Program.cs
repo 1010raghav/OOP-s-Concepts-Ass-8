6fde4b97525782dea8cced7629f9adb22f00f918
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:15:51 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("UC5 output");
+
+            UC5_CalculateWagesFor20DaysInMonth obj5 = new UC5_CalculateWagesFor20DaysInMonth();
+            obj5.CalculateWagesFor20DaysInMonth();
+            
+           } 
+     }         
+} 
