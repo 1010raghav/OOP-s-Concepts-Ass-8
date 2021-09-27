36f2afadc40cde1eeab9a22e23f65207edc5adfa
1010raghav <89019832+1010raghav@users.noreply.github.com>
2021-09-27 21:11:42 +0530

    Program file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Employee_wage_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("UC1 output");
+           
+            UC1_EmployeeAttendance obj1 = new UC1_EmployeeAttendance(); 
+            obj1.GetAttendance();
+            
+           } 
+     }         
+}     
